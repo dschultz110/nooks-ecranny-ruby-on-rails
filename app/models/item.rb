34844f3a859b77cd74f7df0dc11e52a601c5c00d
Dana Schultz <dana.schultz@outlook.com>
2020-11-06 11:53:07 -0600

    Edit items' variants in dashboard

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,8 @@ class Item < ApplicationRecord
   belongs_to :type
   has_many :item_variants
   has_many :variants, through: :item_variants
+  accepts_nested_attributes_for :item_variants, allow_destroy: true
+
   has_many :order_items
   has_many :orders, through: :order_items
 
