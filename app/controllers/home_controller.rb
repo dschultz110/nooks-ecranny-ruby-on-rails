cd324c2bfc42474e95119d53044a69eb8508110b
Dana Schultz <dana.schultz@outlook.com>
2020-11-16 15:39:29 -0600

    Add new and recently updated items to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @items = Item.limit(5).includes(:tag, :type, :variants)
-  end
-
-  def cart
-
+    new_range = Time.now - 5.day
+    updated = Time.now - 1.hour
+    @new_items = Item.where(["created_at >= ? AND updated_at <= ?", new_range, updated]).limit(4)
+    @updated_items = Item.where(["updated_at >= ? AND created_at <= ?", updated, updated]).limit(4)
   end
 end
